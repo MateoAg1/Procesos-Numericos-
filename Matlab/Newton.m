0580307c3f3881087e2ebb6c544e1e629e5ea77b
santisromerot <131311354+santisromerot@users.noreply.github.com>
2023-04-19 19:21:14 -0500

    Update Newton.m

--- Matlab/Newton.m
+++ Matlab/Newton.m
@@ -1,36 +1,36 @@
-% Definimos la función y su derivada
+% Define the function and its derivative
 f = @(x) x^3 - 2*x - 5;
 df = @(x) 3*x^2 - 2;
 
-% Definimos el punto inicial y la tolerancia
+% Define the starting point and tolerance
 x0 = 2;
 tol = 1e-6;
 
-% Inicializamos el contador de iteraciones y el error
+% Initialize the iteration counter and error
 iter = 0;
 err = inf;
 
-% Imprimimos la tabla inicial
+% Print the initial table
 fprintf('Iter\t x\t\t f(x)\t\t err\n');
 fprintf('%d\t %f\t %e\t %e\n', iter, x0, f(x0), err);
 
-% Iteramos hasta que el error sea menor que la tolerancia
+% Iterate until error is less than tolerance
 while err > tol
-    % Calculamos la siguiente aproximación
+    % We calculate the following approximation
     x = x0 - f(x0) / df(x0);
     
-    % Calculamos el error relativo
+    % We calculate the relative error
     err = abs((x - x0) / x);
     
-    % Actualizamos el punto inicial
+    % We update the starting point
     x0 = x;
     
-    % Incrementamos el contador de iteraciones
+    % Increment the iteration counter
     iter = iter + 1;
     
-    % Imprimimos la tabla
+    % We print the table
     fprintf('%d\t %f\t %e\t %e\n', iter, x, f(x), err);
 end
 
-% Imprimimos el resultado final
-fprintf('La raíz aproximada es x = %f\n', x);
+% Print the final result
+fprintf('La raíz aproximada es / The approximate root is x = %f\n', x);
